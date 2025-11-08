9cdf688f0ae27f040d17f6f794f2f6884fbd0e21
hocnghiemtuc <dominhduc258@gmail.com>
2025-11-08 11:07:05 +0700

    Update login page

--- resources/views/web/auth/login.blade.php
+++ resources/views/web/auth/login.blade.php
@@ -7,17 +7,30 @@
             <div class="col-lg-12">
                 <div class="page-wrapper">
                     <div class="row">
+                        {{-- Thông báo lỗi / thành công --}}
                         @if(session('error'))
-                        <div class="col-lg-12">
-                            <div class="alert alert-danger">{{ session('error') }}</div>
-                        </div>
+                            <div class="col-lg-12">
+                                <div class="alert alert-danger">{{ session('error') }}</div>
+                            </div>
                         @endif
+                        @if(session('success'))
+                            <div class="col-lg-12">
+                                <div class="alert alert-success">{{ session('success') }}</div>
+                            </div>
+                        @endif
+
                         <div class="col-lg-12">
-                            <form class="form-wrapper" action="{{ route('web.auth.login') }}"  method="post">
+                            <form class="form-wrapper" action="{{ route('web.auth.login') }}" method="post">
                                 @csrf
-                                <input type="text" name="email" class="form-control" placeholder="Email address">
-                                <input type="password" name="password" class="form-control" placeholder="Password">
+                                <input type="email" name="email" class="form-control" placeholder="Email address" required>
+                                <input type="password" name="password" class="form-control" placeholder="Password" required>
                                 <button type="submit" class="btn btn-primary">Login</button>
+
+                                {{-- Liên kết sang trang đăng ký (đặt cùng vị trí với trang register) --}}
+                                <p class="mt-3">
+                                    Don't have an account?
+                                    <a href="{{ route('web.auth.register.form') }}">Register here</a>
+                                </p>
                             </form>
                         </div>
                     </div>
