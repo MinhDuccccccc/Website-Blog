3209363b5b0dd97bc0ae7033c6a8fe40e52f2a54
hocnghiemtuc <dominhduc258@gmail.com>
2025-12-11 23:18:42 +0700

    Pagination user list

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -1,4 +1,9 @@
 @extends('admin.layout.master')
+
+@section('title')
+  User
+@endsection
+
 @section('content')
 <div id="page-wrapper">
     <div class="container-fluid">
@@ -8,13 +13,14 @@
                     <small>List</small>
                 </h1>
             </div>
+
             @if(session('success'))
                 <div class="alert alert-success">
-                    {{session('success')}}
+                    {{ session('success') }}
                 </div>
             @endif
-            <!-- /.col-lg-12 -->
-            <table class="table table-striped table-bordered table-hover">
+
+            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                 <thead>
                     <tr align="center">
                         <th>ID</th>
@@ -25,25 +31,30 @@
                         <th>Edit</th>
                     </tr>
                 </thead>
+
                 <tbody>
                     @foreach($users as $user)
-                        <tr class="odd gradeX" align="center">
-                            <td>{{$user ->id}}</td>
-                            <td>{{$user ->name}}</td>
-                            <td>{{$user ->email}}</td>
-                            <td>{{$user ->is_admin ? "x" : " "}}</td>
-                            <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="{{route('admin.user.delete', $user -> id)}}"> Delete</a></td>
-                            <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="{{route('admin.user.edit', $user -> id)}}">Edit</a></td>
-                        </tr>
+                    <tr class="odd gradeX" align="center">
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->is_admin ? "x" : "" }}</td>
+
+                        <td class="center">
+                            <i class="fa fa-trash-o fa-fw"></i>
+                            <a href="{{ route('admin.user.delete', $user->id) }}">Delete</a>
+                        </td>
+
+                        <td class="center">
+                            <i class="fa fa-pencil fa-fw"></i>
+                            <a href="{{ route('admin.user.edit', $user->id) }}">Edit</a>
+                        </td>
+                    </tr>
                     @endforeach
                 </tbody>
             </table>
-        </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.container-fluid -->
-</div>
-<!-- /#page-wrapper -->
 
+        </div>
+    </div>
 </div>
 @endsection
