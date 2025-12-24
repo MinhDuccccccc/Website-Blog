80b579f988e73b0d9f435f51441f36496b4f08d9
hocnghiemtuc <dominhduc258@gmail.com>
2025-12-24 22:13:43 +0700

    Update searching

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -158,9 +158,8 @@ public function comment(Request $request, $id)
     $posts = Post::whereRaw(
     "MATCH(title, description, content) AGAINST (? IN BOOLEAN MODE)",
     [$keyword]
-    )->paginate(5);
+    )->paginate(5)->withQueryString();
     $categories = Category::all();
-
     return view('web.search', compact('posts', 'categories', 'keyword'));
 }
 
